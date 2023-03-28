db506c72aae569f253a4e0e0d01083ce329f2b3f
Alain <alain.dossche@vdabcampus.be>
2023-03-28 11:55:16 +0200

    Cursus p 42 start Tweede implementatie

--- src/main/java/be/vdab/theorie/StandaardAfwijking.java
+++ src/main/java/be/vdab/theorie/StandaardAfwijking.java
@@ -1,14 +1,26 @@
 package be.vdab.theorie;
 
 import be.vdab.theorie.exceptions.RepositoryException;
+import be.vdab.theorie.repositories.CsvPersoonRepository;
+import be.vdab.theorie.repositories.JdbcPersoonRepository;
 import be.vdab.theorie.repositories.PersoonRepository;
 import be.vdab.theorie.services.PersoonService;
 
+import java.util.Scanner;
+
 public class StandaardAfwijking {
     public static void main(String[] args) {
+        System.out.print("1=weddes.csv, 2=database:");
+        var scanner = new Scanner(System.in);
+        var keuze = scanner.nextInt();
+        while (keuze != 1 && keuze != 2) {
+            System.out.print("Verkeerd keuze, probeer opnieuw:");
+            keuze = scanner.nextInt();
+        }
+        PersoonRepository repository = keuze == 1 ? new CsvPersoonRepository() :
+                new JdbcPersoonRepository();
         try {
-// ook hier dependency injection zoals bij  hier boven:
-            var service = new PersoonService(new PersoonRepository());
+            var service = new PersoonService(repository);
             System.out.println(service.standaardAfwijkingWeddes());
         } catch (RepositoryException ex) {
             System.err.println("Kan personen niet lezen.");
---
+++ src/main/java/be/vdab/theorie/repositories/JdbcPersoonRepository.java
@@ -0,0 +1,29 @@
+package be.vdab.theorie.repositories;
+
+import be.vdab.theorie.exceptions.RepositoryException;
+
+import java.math.BigDecimal;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class JdbcPersoonRepository extends AbstractRepository implements PersoonRepository{
+    public List<BigDecimal> findAllWeddes() {
+        try {
+            var weddes = new ArrayList<BigDecimal>();
+            var sql = """
+select wedde
+from personen
+""";
+            try (var connection = super.getConnection();
+                 var statement = connection.prepareStatement(sql)) {
+                for (var result = statement.executeQuery(); result.next(); ) {
+                    weddes.add(result.getBigDecimal("wedde"));
+                }
+            }
+            return weddes;
+        } catch(SQLException ex) {
+            throw new RepositoryException(ex);
+        }
+    }
+}
